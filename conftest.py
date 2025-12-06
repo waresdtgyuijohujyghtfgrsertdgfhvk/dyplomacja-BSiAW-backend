9bdcf3c0aba95b252ce09ef328282815779dd20b
wafel-owocowy <18188674346+wafel-owocowy@users.noreply.github.com>
2025-12-06 20:37:06 +0100

    add player setup fixture

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from app import create_app, db
+from app import create_app, db as _db
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -11,21 +11,42 @@ def app():
     app.config.update({'TESTING': True})
     yield app
 
+
 @pytest.fixture()
 def client(app):
     return app.test_client()
 
+
 @pytest.fixture(scope='session')
 def app_ctx(app):
     with app.app_context():
-        yield
+        yield app.app_context()
+
 
 
 @pytest.fixture(scope='session')
 def db(app, app_ctx):
-    db.app = app
-    db.create_all()
+    _db.app = app
+    with app.app_context():
+        _db.create_all()
 
-    yield db
+    yield _db
 
-    db.drop_all()
+    _db.session.close()
+    _db.drop_all()
+
+@pytest.fixture(scope='session')
+def users(app, db):
+    client = app.test_client()
+    user = {"username": "userA", "password": "test_2025A"}
+    response = client.post('/api/register', json=user, content_type='application/json')
+    user = {"username": "userB", "password": "test_2025B"}
+    response = client.post('/api/register', json=user, content_type='application/json')
+    user = {"username": "userC", "password": "test_2025C"}
+    response = client.post('/api/register', json=user, content_type='application/json')
+    user = {"username": "userD", "password": "test_2025D"}
+    response = client.post('/api/register', json=user, content_type='application/json')
+    user = {"username": "userE", "password": "test_2025E"}
+    response = client.post('/api/register', json=user, content_type='application/json')
+    user = {"username": "userF", "password": "test_2025F"}
+    response = client.post('/api/register', json=user, content_type='application/json')
