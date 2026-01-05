fd414917af3be2ca79536679b23c003dec8e897d
wafel-owocowy <18188674346+wafel-owocowy@users.noreply.github.com>
2026-01-05 22:59:30 +0100

    auto arbitration 3/?

--- app/arbitration.py
+++ app/arbitration.py
@@ -16,6 +16,7 @@ def auto_arbitration():
             turns_to_arbitrate.append(Turn.query.filter(Turn.game_id == game.id).order_by(Turn.id.desc()).first())
         if len(turns_to_arbitrate) == 0:
             return
+        print(turns_to_arbitrate)
         adjacencies = {"adr":[("ven","f"),("tri","f"),("ven","f"),("alb","f"),("ion","f"),("apu","f")],
 "aeg":[("gre","f"),("bul","f"),("con","f"),("smy","f"),("eas","f"),("ion","f")]}
         def check_adjacency(src,dst,type):
@@ -38,11 +39,10 @@ def auto_arbitration():
                     board_state.update({row[0]: (row[1], '')})
                     continue
                 board_state.update({row[0]: (row[1], row[2])})
-            print(board_state)
-            nations:list[Nation] = Nation.query.filter(Nation.game_id == game.id).all()
+            nations:list[Nation] = Nation.query.filter_by(game_id = turn.game_id).all()
             nation_owners = dict()
             for nation in nations:
-                nation_owners.update({nation.user_id:nation.name[:3].lower()})
+                nation_owners.update({nation.id:nation.name[:3].lower()})
             orders:list[Orders] = Orders.query.filter(Orders.turn_id == turn.id).all()
             valid_attacks = []
             valid_supports = []
@@ -52,6 +52,7 @@ def auto_arbitration():
             if turn.phase == "spring":
                 for order in orders:
                     order_parts = order.payload.split(" ")
+                    print(order_parts)
                     # invalid order checks
                     if len(order_parts) < 3:
                         continue
@@ -64,24 +65,24 @@ def auto_arbitration():
                         continue
                     if order_source[1][0] == "a" and order_parts[0] == "F":
                         continue
-                    if nation_owners[Orders.player_id] not in order_source[1]:
+                    if nation_owners[order.player_id] not in order_source[1]:
                         continue
                     if order_parts[2] == "h" or order_parts[2] == "holds":
                         holds.append(order)
                         continue
-                    if order_parts[2] == "-" or order_parts[2] == "->":
+                    if order_parts[2] == "-" or order_parts[2] == "->": # syntax: <unit type F/A> <source province> - <destination province>
                         if not check_adjacency(order_parts[1], order_parts[3], order_parts[0]):
                             continue
                         valid_attacks.append(order)
                     if len(order_parts) < 4:
                         continue
-                    if order_parts[2] == "r" or order_parts[2] == "rei":
+                    if order_parts[2] == "r" or order_parts[2] == "rei": # syntax: <unit type F/A> <source_province> r <reinforced unit type F/A> <reinforced province>
                         if not check_adjacency(order_parts[1], order_parts[4], order_parts[0]):
                             continue
                         valid_reinforcements.append(order)
                     if len(order_parts) < 5:
                         continue
-                    if order_parts[2] == "s" or order_parts[2] == "sup":
+                    if order_parts[2] == "s" or order_parts[2] == "sup": # syntax: <unit type F/A> <source province> s <supported unit type F/A> <supported unit source> <supported unit destination>
                         if not check_adjacency(order_parts[1], order_parts[4], order_parts[0]):
                             continue
                         if not check_adjacency(order_parts[1], order_parts[5], order_parts[0]):
@@ -96,56 +97,56 @@ def auto_arbitration():
                         if not check_adjacency(order_parts[1], order_parts[5], order_parts[0]):
                             continue
                         valid_convoys.append(order)
-                for attack in valid_attacks:
-                    pass
+                successful_supports = []
+                for support in valid_supports:
+                    support_order_parts:list[str] = support.payload.split(" ")
+                    for attack in valid_attacks:
+                        attack_order_parts:list[str] = attack.payload.split(" ")
+                        if support_order_parts[1] == attack_order_parts[3]:
+                            continue
+                    successful_supports.append(support)
+                successful_reinforcements = []
+                for reinforcement in valid_reinforcements:
+                    reinforcement_order_parts: list[str] = reinforcement.payload.split(" ")
+                    for attack in valid_attacks:
+                        attack_order_parts: list[str] = attack.payload.split(" ")
+                        if reinforcement_order_parts[1] == attack_order_parts[3]:
+                            continue
+                    successful_reinforcements.append(support)
+                # attack resolution
+                while len(valid_attacks) > 0:
+                    for province, state in board_state.items():
+                        province_candidates:list[tuple[str,int]] = []
+                        for attack in valid_attacks:
+                            attack_order_parts:list[str] = attack.payload.split(" ")
+                            if province == attack_order_parts[3]:
+                                attack_strength = 1
+                                defense_strength = 0
+                                for support in successful_supports:
+                                    support_order_parts: list[str] = support.split(" ")
+                                    if support_order_parts[4] == attack_order_parts[1] and support_order_parts[5] == attack_order_parts[3]:
+                                        attack_strength+=1
+                                province_candidates.append((attack_order_parts[1],attack_strength))
+                                valid_attacks.remove(attack)
+                            if board_state[province][1] != "":
+                                defense_strength = 1
+                                for reinforcement in successful_reinforcements:
+                                    reinforcement_order_parts: list[str] = reinforcement.payload.split(" ")
+                                    if reinforcement_order_parts[4] == province:
+                                        defense_strength+=1
+                            province_candidates.append((province,defense_strength))
+                        max_strength = max([x[1] for x in province_candidates])
+                        if [x[1] for x in province_candidates].count(max_strength) > 1:
+                            continue
+                        for candidate in province_candidates:
+                            if candidate[1] == max_strength:
+                                new_state = {province:(state[0],board_state[candidate[0]][1]),candidate[0]:(board_state[candidate[0]][1],"")}
+                                board_state.update(new_state)
+
+
             if turn.phase == "spring-disband":
                 pass
             if turn.phase == "fall":
-                for order in orders:
-                    order_parts = order.payload.split(" ")
-                    # invalid order checks
-                    if len(order_parts) < 3:
-                        continue
-                    if order_parts[0] == "B":
-                        continue
-                    order_source = board_state[order_parts[1]]
-                    if order_source[1] == "":
-                        continue
-                    if order_source[1][0] == "f" and order_parts[0] == "A":
-                        continue
-                    if order_source[1][0] == "a" and order_parts[0] == "F":
-                        continue
-                    if nation_owners[Orders.player_id] not in order_source[1]:
-                        continue
-                    if order_parts[2] == "h" or order_parts[2] == "holds":
-                        valid_orders.append(order)
-                        continue
-                    if order_parts[2] == "-" or order_parts[2] == "->":
-                        if not check_adjacency(order_parts[1], order_parts[3], order_parts[0]):
-                            continue
-                        valid_orders.append(order)
-                    if len(order_parts) < 4:
-                        continue
-                    if order_parts[2] == "r" or order_parts[2] == "rei":
-                        if not check_adjacency(order_parts[1], order_parts[4], order_parts[0]):
-                            continue
-                        valid_orders.append(order)
-                    if len(order_parts) < 5:
-                        continue
-                    if order_parts[2] == "s" or order_parts[2] == "sup":
-                        if not check_adjacency(order_parts[1], order_parts[4], order_parts[0]):
-                            continue
-                        if not check_adjacency(order_parts[1], order_parts[5], order_parts[0]):
-                            continue
-                        if not check_adjacency(order_parts[4], order_parts[5], order_parts[3]):
-                            continue
-                        valid_orders.append(order)
-
-                    if order_parts[2] == "c" or order_parts[2] == "con":
-                        if not check_adjacency(order_parts[1], order_parts[4], order_parts[0]):
-                            continue
-                        if not check_adjacency(order_parts[1], order_parts[5], order_parts[0]):
-                            continue
-                        valid_orders.append(order)
+                pass
             if turn.phase == "fall-disband":
                 pass
