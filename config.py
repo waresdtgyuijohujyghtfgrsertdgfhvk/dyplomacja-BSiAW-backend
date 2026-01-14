798278f9d0a0ddf1cdfba1548bb4081c1fedd5ea
wafel-owocowy <279462@student.pwr.edu.pl>
2026-01-14 13:33:08 +0100

    moved domain usr to config var

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "8f7b2c019b864a3b9eac53f4d8a4df12")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 REMEMBER_COOKIE_DURATION = timedelta(hours=4)
 PERMANENT_SESSION_LIFETIME = timedelta(hours=4)
+DOMAIN = os.environ.get("DOMAIN","eternalsummer.cc")
